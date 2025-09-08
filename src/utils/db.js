6250684b2c198523cbfad16c51e90ea93f294691
Anastasiya145 <asiva@ukr.net>
2025-09-08 21:50:15 +0300

    one more try

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,9 +4,16 @@ require("dotenv/config");
 const pg = require("pg");
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
+const { DB_HOST, DB_NAME, DB_USERNAME, DB_DIALECT, DB_PASSWORD, DB_PORT } = process.env;
+
+const client = new Sequelize({
+  host: DB_HOST,
+  database: DB_NAME,
+  username: DB_USERNAME,
+  dialect: DB_DIALECT,
   dialectModule: pg,
+  password: DB_PASSWORD,
+  port: DB_PORT,
   dialectOptions: {
     ssl: {
       require: true,
@@ -15,4 +22,4 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-module.exports = sequelize;
+module.exports = client;
